ce94403f4da451510b8ab1b7e881a2a75c6bd789
korypf <korypf@gmail.com>
2019-06-21 13:29:40 +0100

    actualizacion E4 21/06

--- MF0491_3 Cliente/UF1842/E4/js/javascript.js
+++ MF0491_3 Cliente/UF1842/E4/js/javascript.js
@@ -1,9 +1,25 @@
+//boton click aqui
 window.onload = function() {
     document.getElementById('lanzar_alerta').onclick = function () {
     alert('holiiiiis :3');
     }
 }
+/* ALERT inicio página que funciona pero ya no voy a usar
+alert("holis! :3")
+*/
 
+//Para ingresar, guardar y mostrar tu nombre en "Bienvenido"
 var nombre;
 nombre=prompt("Ingresa tu nombre");
-document.write(nombre);
+//document.write(nombre);
+var text = document.createTextNode(nombre); 
+document.getElementById("bv").appendChild(text);
+alert("Que disfrutes de tu paseo por las estrellas " + nombre);
+
+/*agregartxt1();
+
+function agregartxt1(){                
+var text = document.createTextNode(nombre);                                    
+document.getElementById("bv").appendChild(text); 
+*/
+
